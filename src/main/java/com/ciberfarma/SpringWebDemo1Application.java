7da11d0788523a8d9e78a4394ee344ce01c6b2aa
Franco Atme <i202507429@cibertec.edu.pe>
2026-06-03 21:37:25 -0500

    Creación del proyecto y del Index.html

---
+++ src/main/java/com/ciberfarma/SpringWebDemo1Application.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemo1Application.class, args);
+	}
+
+}
