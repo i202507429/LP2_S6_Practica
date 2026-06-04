2aa253d0e23691845953a0e873a0e86f7be1ff38
User <User@DESKTOP-GRVU9PJ>
2026-06-03 22:28:00 -0500

    Implementacion del formulario del controlador

---
+++ src/main/java/com/ciberfarma/controller/ProyectoController.java
@@ -0,0 +1,20 @@
+package com.ciberfarma.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("proyecto")
+public class ProyectoController {
+
+	@PostMapping("saludar")
+	public String saludar(@RequestParam String nombre, Model model) {
+		model.addAttribute("nombre", nombre);
+		return "saludo";
+	}
+}
